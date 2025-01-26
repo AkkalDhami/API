5fa146a8d2f73372e39be312a5d6c98a7d100885
Aavash Dhami <dhamiakkal21@gmail.com>
2025-01-26 22:11:29 +0545

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,6 @@ try {
 }
 }
 main();
+
+
+//{"continent":"Asia","country":"Nepal","zipCode":null,"accuracyRadius":1000,"flag":"https://ipworld.info/static/flags/np.png","city":"Kathmandu","timezone":"Asia/Kathmandu","latitude":27.7142,"countryGeoNameId":1282988,"gmt":"(GMT+05:45) Katmandu","network":"43.245.84.0/22","currencyName":"Nepalese Rupee","countryNativeName":"नपल","stateGeoNameId":12095449,"phoneCode":"+977","state":"Province 3","continentCode":"AS","longitude":85.3145,"currencyNamePlural":"Nepalese rupees","cityGeoNameId":1283240,"languages":"ne","numOfCities":56,"org":"Vianet Communications Pvt. Ltd.","ip":"43.245.86.115","currencySymbol":"NPRs","currencySymbolNative":"नेरू","isEU":"No","countryTLD":".np","countryCapital":"Kathmandu","metroCode":null,"continentGeoNameId":6255147,"stateCode":"P3","countryISO2":"NP","numOfStates":19,"countryISO3":"NPL","currencyCode":"NPR","asNo":45650,"status":200}
